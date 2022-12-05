539f088fbb5713b7e499454ab7314daf74eeea3d
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2022-12-05 14:03:40 -0500

    Correctly order pitches

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const playSound = (sound) => {
 }
 
 pianoKeys.forEach((pianoKey, i) => {
-    const soundIndex = i < 9 ? '0' + (i + 1) : (i+1);
-    const whichSound = `24-piano-keys/key${soundIndex}.mp3`;
-    
+    const orderedSounds = ['09','11','14','16','18','21','23','02','04','06','08','10','12','13','15','17','19','20','22','24','01','03','05','07']
+    const whichSound = `24-piano-keys/key${orderedSounds[i]}.mp3`;
+
     pianoKey.addEventListener('click', () => playSound(whichSound));
 })
